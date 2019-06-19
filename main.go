a1004e8bc8c934c27e87f6f1f8296d3c122dbacc
Bennett <mbennett5@allegisgroup.com>
2019-06-19 11:21:11 -0500

    react app being served by go

--- main.go
+++ main.go
@@ -1,14 +1,28 @@
 package main
 
+import (
+	"net/http"
+
+	"github.com/gin-gonic/contrib/static"
+	"github.com/gin-gonic/gin"
+)
+
 func main() {
-	cards := newDeck()
-	var hand deck
-	cards.print()
-	cards.shuffleDeck()
-	cards.print()
-	cards, hand = deal(cards, 5)
-	hand.print()
-	cards.print()
+	// Set the router as the default one shipped with Gin
+	router := gin.Default()
+	// Serve frontend static files
+	router.Use(static.Serve("/", static.LocalFile("./client/build", true)))
+	// Setup route group for the API
+	api := router.Group("/api")
+	{
+		api.GET("/", func(c *gin.Context) {
+			c.JSON(http.StatusOK, gin.H{
+				"message": "pong",
+			})
+		})
+	}
+	// Start and run the server
+	router.Run(":5000")
 }
 
 //to run this project, type "go run main.go deck.go card.go" in the terminal.
